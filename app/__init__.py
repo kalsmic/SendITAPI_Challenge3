c3c4f829f62fa751cefeda55caa75fc76a560ff2
Kalule Arthur <kalulearthur@gmail.com>
2018-11-19 18:39:41 +0300

    Set up database for the app [#162035343]

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+from config import app_config
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+
+    return app
